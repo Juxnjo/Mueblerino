beb82220374813731b74f014e55e5158a433c2b3
“Juan <ramirezjuanj0117@gmail.com>
2023-06-15 02:14:07 -0500

    interpolando imagenes otra vez

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,15 +1,16 @@
 import "./gallery.css"
+import header from '/src/assets/Header.jpeg'
 
 export const Gallery = () => {
   return (
     <div className="galeria">
         <h1 className='font-bold'>Galeria</h1>
         <div className="portafolio">
-        <img className='border-none rounded-lg' src="/src/assets/Header.jpeg" alt="" />
-        <img className='border-none rounded-lg' src="/src/assets/Header.jpeg" alt="" />
-        <img className='border-none rounded-lg' src="/src/assets/Header.jpeg" alt="" />
-        <img className='border-none rounded-lg' src="/src/assets/Header.jpeg" alt="" />
-        <img className='border-none rounded-lg' src="/src/assets/Header.jpeg" alt="" />
+        <img className='border-none rounded-lg' src={header} alt="" />
+        <img className='border-none rounded-lg' src={header} alt="" />
+        <img className='border-none rounded-lg' src={header} alt="" />
+        <img className='border-none rounded-lg' src={header} alt="" />
+        <img className='border-none rounded-lg' src={header} alt="" />
         </div>
     </div>
   )
--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -1,4 +1,5 @@
 import './ourservices.css'
+import header from '/src/assets/Header.jpeg'
 
 export const OurServices = () => {
   return (
@@ -6,19 +7,19 @@ export const OurServices = () => {
         <div className="servicios">
             <h1 className='font-bold'>Servicios</h1>
             <div className='servicio'>
-              <img className='border-none rounded-lg' src="/src/assets/Header.jpeg" alt="" />
+              <img className='border-none rounded-lg' src={header} alt="" />
               <h1 className='font-semibold'>Decoración de interiores</h1>
               <p>Mejora la apariencia de tu hogar o negocio con nuestro servicio de decoración de interiores diseñado a medida.</p>
               <button className='border-2 rounded-lg font-medium'>Info</button>
             </div>
             <div className='servicio'>
-              <img className='border-none rounded-lg' src="/src/assets/Header.jpeg" alt="" />
+              <img className='border-none rounded-lg' src={header} alt="" />
               <h1 className='font-semibold'>Diseño de espacios</h1>
               <p>Transforma tus espacios en áreas impresionantes y prácticas con nuestro servicio de diseño de espacios personalizado.</p>
               <button className='border-2 rounded-lg font-medium'>Info</button>
             </div>
             <div className='servicio'>
-              <img className='border-none rounded-lg' src="/src/assets/Header.jpeg" alt="" />
+              <img className='border-none rounded-lg' src={header} alt="" />
               <h1 className='font-semibold'>Asesoramiento en diseño</h1>
               <p>Te ayudamos a tomar las mejores decisiones en cuanto al diseño de tu hogar o negocio, ofreciendo una asesoría especializada y personalizada.</p>
               <button className='border-2 rounded-lg font-medium'>Info</button>
